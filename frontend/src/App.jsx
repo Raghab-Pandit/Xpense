94217676a18cf0da5442cd7481b2b6298968f4e8
raghab-pandit01 <raghab.pandit@w3developers.com.np>
2025-12-12 11:46:53 +0545

    Defined App Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Navigate,
+  Route
+} from 'react-router-dom'
+import Home from './pages/Dashboard/Home'
+import Income from './pages/Dashboard/Income'
+import Expenses from './pages/Dashboard/Expenses'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
 function App() {
 
   return (
-    <div className='text-5xl font-bold'>
-    App
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expenses" element={<Expenses />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
 export default App
+
+const Root = () => {
+  //check if user is authenticated
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  //redirect to dashboard if authenticated, else to login
+  return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />;
+}
